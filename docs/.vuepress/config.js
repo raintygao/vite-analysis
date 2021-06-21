4d259c00c19472fa5bbaef5b86fce1270c7be2cd
raintygao <726848199@qq.com>
2021-06-21 10:53:24 +0800

    feat: add seo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -34,5 +34,10 @@ module.exports = {
       }
     }
   },
+  head: [
+    ['meta', { name: 'keywords', content: 'Vite 技术揭秘, 前端 构建工具,Vue.js,源码解析,源码分析,打包工具' }],
+    ['meta', { property: 'og:title', content: 'Vite 技术揭秘' }],
+    ['meta', { property: 'og:description', content: '构建工具Vite及其生态的源码阅读与解析' }],
+  ],
   plugins:['@vuepress/medium-zoom','@vuepress/plugin-back-to-top']
 };
